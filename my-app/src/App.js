08a18607890c520f820ce6b88392ada143d04005
vitalyvitmens <otm_@tut.by>
2023-06-04 14:19:03 +0300

    add header on the page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,17 @@ import styles from './app.module.css'
 export const App = () => {
 	return (
 		<>
+			<div className={styles.header}>
+				<h1>
+					Добро пожаловать!
+					<p className={styles.p}>
+						Выберите одну из ниже предложенных форм регистрации!
+					</p>
+					<p className={styles.p}>
+						Ваш выбор очень важен для нас, пожалуйста сделайте выбор!
+					</p>
+				</h1>
+			</div>
 			<div className={styles.app}>
 				<Registration />
 				<Yup />
