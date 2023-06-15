0d647d693433fb9ab9a6b6473326c5a7a650b90c
vitalyvitmens <otm_@tut.by>
2023-06-15 15:02:29 +0300

    add data in css project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,50 +32,63 @@ export const App = () => {
 	}, [isFormValid])
 
 	return (
-		<div className={styles.app}>
-			<div className={styles.authFormContainer}>
-				<form className={styles.form} onSubmit={onSubmit}>
-					<Field
-						type="email"
-						name={email}
-						placeholder="Почта..."
-						value={email}
-						setValue={setEmail}
-						setIsValid={setIsEmailValid}
-						validators={[emailValidator]}
-					/>
-					<Field
-						type="password"
-						name={password}
-						placeholder="Пароль..."
-						value={password}
-						setValue={setPassword}
-						setIsValid={setIsPasswordValid}
-						validators={[passwordMinValidator, passwordSymbolsValidator]}
-					/>
-					<Field
-						type="password"
-						name={passcheck}
-						placeholder="Повтор пароля..."
-						value={passcheck}
-						setValue={setPasscheck}
-						setIsValid={setIsPasscheckValid}
-						validators={[
-							(value) => (value === password ? null : 'Пароли не совпадают'),
-						]}
-						dependencies={{ password }}
-						forceValidation={(value) => value.length > 0}
-					/>
-					<button
-						className={styles.button}
-						type="submit"
-						disabled={!isFormValid}
-						ref={submitButtonRef}
-					>
-						Зарегистрироваться
-					</button>
-				</form>
+		<>
+			<div className={styles.header}>
+				<h1>
+					Добро пожаловать!
+					<p className={styles.p}>
+						Выберите одну из ниже предложенных форм регистрации!
+					</p>
+					<p className={styles.p}>
+						Ваш выбор очень важен для нас, пожалуйста сделайте выбор!
+					</p>
+				</h1>
 			</div>
-		</div>
+			<div className={styles.app}>
+				<div className={styles.authFormContainer}>
+					<form className={styles.form} onSubmit={onSubmit}>
+						<Field
+							type="email"
+							name={email}
+							placeholder="Почта..."
+							value={email}
+							setValue={setEmail}
+							setIsValid={setIsEmailValid}
+							validators={[emailValidator]}
+						/>
+						<Field
+							type="password"
+							name={password}
+							placeholder="Пароль..."
+							value={password}
+							setValue={setPassword}
+							setIsValid={setIsPasswordValid}
+							validators={[passwordMinValidator, passwordSymbolsValidator]}
+						/>
+						<Field
+							type="password"
+							name={passcheck}
+							placeholder="Повтор пароля..."
+							value={passcheck}
+							setValue={setPasscheck}
+							setIsValid={setIsPasscheckValid}
+							validators={[
+								(value) => (value === password ? null : 'Пароли не совпадают'),
+							]}
+							dependencies={{ password }}
+							forceValidation={(value) => value.length > 0}
+						/>
+						<button
+							className={styles.button}
+							type="submit"
+							disabled={!isFormValid}
+							ref={submitButtonRef}
+						>
+							Зарегистрироваться
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
 	)
 }
